260126f3214725732c4c9126708ebea1f21ac96d
triandamai <triannurizkillah@gmail.com>
2021-02-17 21:34:26 +0700

    feat:move auth method to separate file(Repositry)

--- src/store/Repositoy.ts
+++ src/store/Repositoy.ts
@@ -79,50 +79,81 @@ export const useAuth = () => {
       authState.form.email,
       authState.form.password
     )
-      .then(user => {})
-      .catch(e => {})
-      .finally(() => {});
+      .then(user => {
+        if (user.user?.uid)
+          setUser(
+            {
+              username: authState.form.username,
+              email: authState.form.email,
+              password: authState.form.password,
+              uuid: user.user.uid,
+              createdAt: Date.now().toLocaleString(),
+              updatedAt: Date.now().toLocaleString(),
+              authMethod: "basic"
+            },
+            true
+          );
+        else
+          authState.result = {
+            status: false,
+            message: "gagal"
+          };
+      })
+      .catch(e => {
+        authState.result = { status: false, message: `${e}` };
+      })
+      .finally(() => {
+        authState.isLoading = false;
+      });
   };
   const resultFromredirectGoogle = (isLogin: boolean) => {
+    authState.isLoading = true;
     AuthGoogle.getRedirectResult()
       .then(result => {
         if (result.user)
           if (isLogin) changelastLogin(`${result.user?.uid}`);
           else
-            setUser({
-              username: `${result.user?.displayName}`,
-              email: `${result.user?.email}`,
-              password: `${result.user?.uid}`,
-              createdAt: Date.now().toLocaleString(),
-              updatedAt: Date.now().toLocaleString(),
-              authMethod: "google",
-              uuid: result.user?.uid
-            });
-        else authState.status = false;
+            setUser(
+              {
+                username: `${result.user?.displayName}`,
+                email: `${result.user?.email}`,
+                password: `${result.user?.uid}`,
+                createdAt: Date.now().toLocaleString(),
+                updatedAt: Date.now().toLocaleString(),
+                authMethod: "google",
+                uuid: result.user?.uid
+              },
+              false
+            );
+        else authState.result = { status: false, message: "Gagal" };
       })
       .catch(e => {
-        authState.status = false;
+        authState.result = {
+          status: false,
+          message: `${e}`
+        };
+      })
+      .finally(() => {
+        authState.isLoading = false;
       });
   };
 
-  const setUser = (user: User) => {
+  const setUser = (user: User, shouldLogin: boolean) => {
     dbUser
       .doc(user.uuid)
       .set(user)
-      .then(result => {
+      .then(() => {
         router.push({ path: "/dashboard" });
-      })
-      .catch(e => {});
+      });
   };
 
   const changelastLogin = (uuid: string) => {
     dbUser
       .doc(uuid)
       .set({ updatedAt: Date.now().toLocaleString() })
-      .then(result => {
+      .then(() => {
         router.push({ path: "/dashboard" });
-      })
-      .catch(e => {});
+      });
   };
 
   return {
