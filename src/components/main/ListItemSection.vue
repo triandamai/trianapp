498d21927e12453cf7c95d201bfad04fb140901b
triandamai <triannurizkillah@gmail.com>
2021-02-20 00:34:54 +0700

    refactor: make all  component base

---
+++ src/components/main/ListItemSection.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ListItemSection",
+});
+</script>
+<template>
+  <div class="flex flex-wrap -mx-4 -mt-4 -mb-10 sm:-m-4">
+    <slot></slot>
+  </div>
+</template>
--- src/views/Tutorial.vue
+++ src/views/Tutorial.vue
@@ -3,8 +3,11 @@ import { defineComponent, onMounted } from "vue";
 import { useStore } from "@/store";
 
 import { DocumentActionTypes } from "@/store/module/action-types";
+import HeaderSection from "@/components/main/HeaderSection.vue";
+import ListItemSection from "@/components/main/ListItemSection.vue";
 
 export default defineComponent({
+  components: { HeaderSection, ListItemSection },
   setup() {
     const store = useStore();
     onMounted(() => {
@@ -17,44 +20,26 @@ export default defineComponent({
 });
 </script>
 <template>
-  <div
-    class="w-full h-full font-mono bg-gray-100 min-h-max dark:bg-gray-900"
-    style="height: 80%"
-  >
-    <section class="text-gray-900 dark:text-gray-200">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="mb-20 text-center">
-          <h1
-            class="mb-4 font-mono text-4xl font-extrabold leading-10 tracking-tight title-font sm:text-5xl sm:leading-none md:text-6xl"
-          >
-            Tutorial
-          </h1>
-          <p
-            class="mx-auto font-mono text-base leading-relaxed xl:w-2/4 lg:w-3/4"
-          >
-            Informasi seputar teknologi. membantu para developer mendapatkan
-            informasi dunia teknologi.
-          </p>
-          <div class="flex justify-center mt-6">
-            <div class="inline-flex w-16 h-1 bg-indigo-500 rounded-full"></div>
-          </div>
-        </div>
-        <div class="flex flex-wrap -mx-4 -mt-4 -mb-10 sm:-m-4">
-          <!-- CARD -->
-          <card-tutorial
-            v-for="(item, index) in store.state.tutorial"
-            :data="item"
-            :key="index"
-          />
-        </div>
-      </div>
-    </section>
-  </div>
+  <section class="text-gray-900 dark:text-gray-200">
+    <div class="container px-5 py-24 mx-auto">
+      <header-section :title="Tutorial">
+        <p
+          class="mx-auto font-mono text-base leading-relaxed xl:w-2/4 lg:w-3/4"
+        >
+          Informasi seputar teknologi. membantu para developer mendapatkan
+          informasi dunia teknologi.
+        </p>
+      </header-section>
+      <list-item-section>
+        <card-tutorial
+          v-for="(item, index) in store.state.tutorial"
+          :data="item"
+          :key="index"
+        />
+      </list-item-section>
+    </div>
+  </section>
 </template>
-  <style  scoped>
-.w-70 {
-  width: 20rem;
-}
-</style>
+
 
 
