b83655c15e24b24080e12e9822e68448743c9dc0
Trian <triannurizkillah@gmail.com>
2021-04-01 10:53:21 -0400

    feat(home): adds icons

--- src/components/HomeHeader.vue
+++ src/components/HomeHeader.vue
@@ -32,7 +32,7 @@
               class="lg:hidden block title-font sm:text-5xl lg:text-6xl text-4xl mb-8 lg::px-10 md:px-10 sm:px-6 px-0 font-semibold sm:leading-tight"
               style="color: #cbcbd2; line-height: 1.2"
             >
-              TTrian Damai
+              Trian Damai
             </h1>
             <div
               class="inline-block items-center mx-auto lg:mx-0 lg:flex justify-center lg:space-x-8 md:space-x-2 sm:space-x-3 space-x-0"
@@ -43,22 +43,24 @@
               >
                 <div class="flex items-center text-white">
                   <svg
-                    class="mr-2.5"
-                    width="13"
-                    height="12"
-                    viewBox="0 0 13 13"
-                    fill="none"
                     xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    width="15"
+                    height="15"
+                    class="mr-5"
+                    fill="currentColor"
                   >
                     <path
-                      d="M10.9293 7.99988L6.66668 5.15788V10.8419L10.9293 7.99988ZM12.9173 8.27722L5.85134 12.9879C5.80115 13.0213 5.74283 13.0404 5.6826 13.0433C5.62238 13.0462 5.5625 13.0327 5.50934 13.0042C5.45619 12.9758 5.41175 12.9334 5.38075 12.8817C5.34976 12.83 5.33337 12.7708 5.33334 12.7105V3.28922C5.33337 3.22892 5.34976 3.16976 5.38075 3.11804C5.41175 3.06633 5.45619 3.02398 5.50934 2.99552C5.5625 2.96706 5.62238 2.95355 5.6826 2.95644C5.74283 2.95932 5.80115 2.97848 5.85134 3.01188L12.9173 7.72255C12.963 7.75299 13.0004 7.79423 13.0263 7.84261C13.0522 7.89099 13.0658 7.94501 13.0658 7.99988C13.0658 8.05475 13.0522 8.10878 13.0263 8.15716C13.0004 8.20553 12.963 8.24678 12.9173 8.27722Z"
-                      fill="#707092"
+                      fill-rule="evenodd"
+                      d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
                     />
                   </svg>
                   Download Resume
                 </div>
               </button>
             </div>
+            <div class="flex text-white p-5"></div>
           </div>
           <!-- Right Column -->
           <div class="w-full lg:w-1/2 text-center justify-center flex pr-0">
