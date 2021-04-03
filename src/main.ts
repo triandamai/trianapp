bfd046bc8e84182ee9529caab19f99cb99b2fe78
Trian <triannurizkillah@gmail.com>
2021-04-03 01:43:48 -0400

    feat(title):change title each router

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { useTitle } from "@vueuse/core";
 import App from "./App.vue";
 import { router } from "./router";
 import "virtual:windi.css";
+import.meta.env.VITE_APP_TITTLE;
 
 router.beforeEach((toRoute: any, fromRoute, next) => {
   useTitle(toRoute.meta.title);
