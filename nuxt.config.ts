406d1384d372d8c7b5fc3531c13ddda0d04a6d56
triandamai <triannurizkillah@gmail.com>
2022-09-07 20:01:22 +0700

    feat: trying deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     ssr:false,
+    nitro:{
+        preset:'service-worker'
+    },
     modules:[
         "@nuxtjs/tailwindcss",
         "@nuxt/content"
