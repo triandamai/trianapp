062630adf6d5823772a2f2ea0bbd7336fdf66498
triandamai <triannurizkillah@gmail.com>
2022-09-07 14:23:18 +0700

    feat: add link social

--- components/About.vue
+++ components/About.vue
@@ -7,10 +7,12 @@
                 <path d="M6 1a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V1Z" class="fill-pink-300">
                 </path>
             </svg><span class="ml-2.5">About</span></h2>
-        <p class="mt-2  text-sm leading-7 text-slate-700 lg:line-clamp-4">In this show, Eric and Wes dig
-            deep to get to the facts with guests who have been labeled villains by a society quick to judge,
-            without actually getting the full story. Tune in every Thursday to get to the truth with another
-            misunderstood outcast as they share the missing context in their tragic tale.</p><button type="button"
+        <p class="mt-2  text-sm leading-7 text-slate-700 lg:line-clamp-4">Talented Android developer with over 3 years
+            of experience in developing software and
+            web in a wide range of industries, health,Delivery service, Solid understanding of full
+            mobile and software development life cycles, UIX, and Agile methodologies. Dedicated
+            to continuously developing, implementing, and adopting new technologies to maximize
+            development efficiency and produce innovative applications.</p><button type="button"
             class="mt-2 hidden text-sm font-bold leading-6 text-pink-500 hover:text-pink-700 active:text-pink-900 lg:inline-block">Show
             more</button>
     </section>
