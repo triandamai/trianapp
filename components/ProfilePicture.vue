edd7bee31ee37f0ab2cde6b3a1a541cbb18a7552
trian damai <triannurizkillah@gmail.com>
2022-09-10 12:29:55 +0700

    feat: add projects and snippets

--- components/ProfilePicture.vue
+++ components/ProfilePicture.vue
@@ -1,11 +1,9 @@
 <template>
     <a class="relative mx-auto block w-48 overflow-hidden rounded-lg bg-slate-200 shadow-xl shadow-slate-200 sm:w-64 sm:rounded-xl lg:w-auto lg:rounded-2xl"
-                    aria-label="Homepage" href="/"><img alt=""
-                        sizes="(min-width: 1024px) 20rem, (min-width: 640px) 16rem, 12rem"
-                        src="https://avatars.githubusercontent.com/u/41132846?v=4"
-                        decoding="async" data-nimg="future" class="w-full" width="960" height="960">
-                    <div
-                        class="absolute inset-0 rounded-lg ring-1 ring-inset ring-black/10 sm:rounded-xl lg:rounded-2xl">
-                    </div>
-                </a>
+        aria-label="Homepage" href="/"><img alt="" sizes="(min-width: 1024px) 20rem, (min-width: 640px) 16rem, 12rem"
+            src="https://avatars.githubusercontent.com/u/41132846?v=4" decoding="async" data-nimg="future"
+            class="w-full" width="960" height="960">
+        <div class="absolute inset-0 rounded-lg ring-1 ring-inset ring-black/10 sm:rounded-xl lg:rounded-2xl">
+        </div>
+    </a>
 </template>
---
+++ pages/snippets/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <SectionContent title="Blogs">
+        <ItemBlog v-for="(item,index) in data" :date="item.publishedAt" :title="item.title" :key="item.slug"
+            :description="item.description" :tags="item.tags" :path="'/blogs/'+item.slug" />
+    </SectionContent>
+</template>
+<script>
+export default {
+  head () {
+    return {
+      title: 'About Trian Damai - Trian.app',
+      meta: [
+        { hid: 'description', name: 'description', content: 'About Trian Damai' }
+      ]
+    }
+  },
+  async asyncData(ctx){
+    const data = await ctx.$content('snippets')
+                  .only([
+                  'title','description','slug','createdAt','updatedAt','tags'
+                  ])
+                  .sortBy('createdAt','desc')
+                  .fetch()
+
+  
+
+    return{data}
+
+  }
+}
+</script>
